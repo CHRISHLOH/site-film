39909eb0cf3251f882ca5c3ba427c97df3642739
CHRISHLOH <john.johnson127721@gmail.com>
2025-04-29 16:54:18 +0300

    Отдельный фронт рабочий

--- user-service/src/main/java/org/sitefilm/userservice/controller/rest/TestController.java
+++ user-service/src/main/java/org/sitefilm/userservice/controller/rest/TestController.java
@@ -1,8 +1,6 @@
 package org.sitefilm.userservice.controller.rest;
 
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
+import lombok.extern.slf4j.Slf4j;;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
