8a3f2cdfe0d13389e4e6044e6105ef879e2e3f3a
CHRISHLOH <john.johnson127721@gmail.com>
2025-07-22 20:40:36 +0300

    Spring Ai откат

--- ai-service/src/main/resources/db/changelog/dev-0.1.0/db.changelog-003.sql
+++ ai-service/src/main/resources/db/changelog/dev-0.1.0/db.changelog-003.sql
@@ -1,10 +1,9 @@
 CREATE TABLE ai_service.spring_ai_chat_memory (
-                                       conversation_id VARCHAR(255) NOT NULL,
-                                       created TIMESTAMP NOT NULL,
-                                       role VARCHAR(50),
-                                       content TEXT
+                                                  conversation_id VARCHAR(255) NOT NULL,
+                                                  content TEXT NOT NULL,
+                                                  type VARCHAR(50) NOT NULL,
+                                                  timestamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
--- Индекс для ускорения поиска по conversation_id
 CREATE INDEX idx_saichatm_conversation
     ON ai_service.spring_ai_chat_memory (conversation_id);
