7369eea9831a65f2966fa31d67b6136a139aed0e
CHRISHLOH <john.johnson127721@gmail.com>
2025-11-29 21:14:02 +0300

    Start etl

---
+++ etl-service/src/main/java/com/sitefilm/etl/service/DownloadService.java
@@ -0,0 +1,11 @@
+package com.sitefilm.etl.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DownloadService {
+
+    public String download() {
+        return "download success";
+    }
+}
