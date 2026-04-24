ffb1b68309c4cc545013f19aedc639f588a49069
CHRISHLOH <john.johnson127721@gmail.com>
2026-04-24 15:17:24 +0300

    Перепись loadStrategy не падение по rateLimit

--- etl-service/src/main/java/com/sitefilm/etl/infrastructure/provider/tmdb/client/RateLimitedTmdbClient.java
+++ etl-service/src/main/java/com/sitefilm/etl/infrastructure/provider/tmdb/client/RateLimitedTmdbClient.java
@@ -8,7 +8,7 @@ import com.sitefilm.etl.infrastructure.provider.tmdb.response.person.PersonDetai
 import com.sitefilm.etl.infrastructure.provider.tmdb.response.movie.PersonsInMovieResponseDto;
 import io.github.resilience4j.ratelimiter.RateLimiter;
 import io.github.resilience4j.ratelimiter.RateLimiterConfig;
-import io.github.resilience4j.ratelimiter.RequestNotPermitted;
+
 import org.springframework.stereotype.Component;
 
 import java.time.Duration;
